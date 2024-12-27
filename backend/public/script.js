6756057a8d6e1da71f2281fb30c4eebe03a56d58
Likhin-HS <likhindluffy@outlook.com>
2024-12-27 12:57:52 +0530

    Refactor login navigation and replace old login.html with new implementation

--- backend/public/script.js
+++ backend/public/script.js
@@ -16,8 +16,8 @@ limitations under the License.
 */
 function navigateTo(page) {
   if (page === 'signup') {
-  window.location.href = '../../frontend/username.html';
+  window.location.href = 'username.html';
   } else if (page === 'login') {
-  window.location.href = '../../frontend/login.html';
+  window.location.href = 'login.html';
   }
 }
